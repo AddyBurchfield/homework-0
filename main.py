14d55d8179bcbe76bd2603cb907d22ff0a4ec163
Addy Burchfield <abb6060@psu.edu>
2020-08-26 20:41:29 +0000

    misspelled celsius

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-temperature = float( input("Enter temperature in celsuis: ") )
+temperature = float( input("Enter temperature in celsius: ") )
 farenheittemperature = ( (9 / 5 ) * temperature ) + 32
 
 print(str( temperature ) +"° in Celsius is equivalent to " + str( farenheittemperature ) + "° Fahrenheit.")
